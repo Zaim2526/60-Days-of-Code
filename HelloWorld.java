4ca9c8db728f5bb245f71bd81cd64585635c0560
Zaim2526 <108755732+Zaim2526@users.noreply.github.com>
2022-12-02 23:04:37 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld{
+  public static void main(String[] args){
+    System.out.println("Hello World, Zamin this side");
+    System.out.println("I wish all the best to all of you for this 60 days of code....");
+  
+  }
+}
