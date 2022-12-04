c0044aa3f52f22eded2dcd0128c3c27ba8a4aa40
Zamin Zehra <108755732+Zayam2526@users.noreply.github.com>
2022-12-04 18:48:31 +0530

    Create SwapNum.java

---
+++ SwapNum.java
@@ -0,0 +1,15 @@
+//java program to swap two numbers.
+
+ class SwapNum {
+    public static void main(String[] args) {
+        int a=100;
+        int b=50;
+        int c;
+        
+        c=a;
+        a=b;
+        b=c;
+        
+        System.out.println("after swap:" +a +" "+b);
+    }
+}
