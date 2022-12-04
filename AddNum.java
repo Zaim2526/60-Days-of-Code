55eb667d72dcda7840f7ce6b2c7533162583c113
Zamin Zehra <108755732+Zayam2526@users.noreply.github.com>
2022-12-04 18:49:06 +0530

    Create AddNum.java

---
+++ AddNum.java
@@ -0,0 +1,10 @@
+// java program to add two numbers.
+
+class AddNum {
+    public static void main(String[] args) {
+        int a=100;
+        int b=50;
+        int c=a+b;
+        System.out.println("Addition  of a and b is :" +c);
+    }
+}
