add277df9083f810bef6dc417ae21af9a902a1f3
Zamin Zehra <108755732+Zayam2526@users.noreply.github.com>
2022-12-05 23:37:16 +0530

    Create ForLoop.java

---
+++ ForLoop.java
@@ -0,0 +1,13 @@
+//Print n numbers using user input.
+import java.util.*;
+class ForLoop {
+    public static void main(String[] args) {
+        System.out.println("Enter the value: ");
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+        for(int i=0;i<=n;i++){
+        System.out.println(i);
+            
+        }
+}
+}
