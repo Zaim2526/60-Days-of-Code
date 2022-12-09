796eb5d0e72f98d0bcccfb056e7209686ad53eb3
Zamin Zehra <108755732+Zayam2526@users.noreply.github.com>
2022-12-09 09:17:42 -0800

    Create Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,29 @@
+package gitcode;
+import java.util.*;
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		System.out.println("Enter the value: ");
+		Scanner sc = new Scanner(System.in);
+				
+		int n= sc.nextInt() ;
+		int a =0;
+		int b = 1;
+		System.out.print(a+" ");
+		System.out.print(b+" ");
+		  
+		for (int i=0;i<n-2;i++) {
+			int c = a+b;
+			
+			System.out.print(c+ " ");
+	
+           a=b;
+           b=c;
+		
+		
+		
+		}
+
+	}
+
+}
