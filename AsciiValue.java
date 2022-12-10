82aa30f2e558730a4bdc8c197a4dfa7fc9f4f1f0
Zamin Zehra <108755732+Zayam2526@users.noreply.github.com>
2022-12-10 08:57:47 -0800

    Create AsciiValue.java

---
+++ AsciiValue.java
@@ -0,0 +1,19 @@
+package gitcode;
+import java.util.*;
+public class AsciiValue {
+
+	public static void main(String[] args) {
+		
+		
+	    char ch ='a';
+	    
+		int ascii = ch;
+		
+		int castAscii = (int)ch;
+		
+		
+		System.out.println("The ASCII value of " + ch + "is: " + ascii);
+		System.out.println("The ASCII value of " + ch + "is: " + castAscii);
+	}
+
+}
