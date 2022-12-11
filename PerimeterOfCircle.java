2e32d80b94df38b7f33c260b23e55115e07f2d51
Zamin Zehra <108755732+Zayam2526@users.noreply.github.com>
2022-12-11 09:32:02 -0800

    Create PerimeterOfCircle.java

---
+++ PerimeterOfCircle.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class PerimeterOfCircle{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		int r=sc.nextInt();
+		float perimeter=2*3.14f*r;
+
+		System.out.println("perimeter of circle"+perimeter);
+	}
+}
