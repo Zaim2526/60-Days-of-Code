73860fa96fda3312cc1d1a8d8bcc88ba551382b9
Zamin Zehra <108755732+Zayam2526@users.noreply.github.com>
2022-12-12 06:25:58 -0800

    Create DisplayAlphabet.java

---
+++ DisplayAlphabet.java
@@ -0,0 +1,14 @@
+// java code to display alphabet using for loop.
+package gitcode;
+
+public class DisplayAlphabet {
+
+	public static void main(String[] args) {
+		char ch;
+		for(ch='A';ch<='Z';ch++) {
+			System.out.println(ch);
+		}
+
+	}
+
+}
