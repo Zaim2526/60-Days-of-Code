3b2a77bcccbb4989c95a0622de6c868bdaaaacb8
Zamin Zehra <108755732+Zayam2526@users.noreply.github.com>
2022-12-12 06:18:29 -0800

    Create Vowelconsonant.java

---
+++ Vowelconsonant.java
@@ -0,0 +1,21 @@
+package gitcode;
+import java.util.*;
+public class Vowelconsonant {
+
+	public static void main(String[] args) {
+		System.out.println("Enter the character: ");
+		Scanner sc = new Scanner(System.in);
+		char ch = sc.next().charAt(0);
+		
+	
+		if(ch == 'a' || ch == 'e' ||ch == 'i' || ch == 'o' || ch == 'u'|| ch =='A'  ||ch == 'E' ||ch == 'I' || ch == 'O' || ch == 'U') {
+			System.out.println(ch + " is vowel");
+			
+		}else
+		{
+			System.out.println(ch + "is consonant");
+		}
+
+	}
+
+}
