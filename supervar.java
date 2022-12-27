0291e90e53600b733595a0dbaefa7156c7e251cc
Zamin Zehra <108755732+Zayam2526@users.noreply.github.com>
2022-12-27 23:42:47 +0530

    Create supervar.java

---
+++ supervar.java
@@ -0,0 +1,19 @@
+class supervar
+{
+	int x=10;
+}
+class demo1 extends supervar
+{
+	int x=20;
+	public void display()
+	{
+		System.out.println(x);
+		System.out.println(super.x);
+		System.out.println(x+super.x);
+	}
+public static void main(String args[])
+	{
+		demo1 d=new demo1();
+		d.display();
+	}
+}
