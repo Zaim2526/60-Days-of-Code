209240474844e179692de0e67e0d17a60c4a82c2
Zamin Zehra <108755732+Zayam2526@users.noreply.github.com>
2022-12-29 23:20:05 +0530

    Create FrequencyCharacter.java

---
+++ FrequencyCharacter.java
@@ -0,0 +1,18 @@
+// Java Program to Find Frequency of Character
+
+public class FrequencyCharacter {
+
+    public static void main(String[] args) {
+        String str = "This website is awesome.";
+        char ch = 'e';
+        int frequency = 0;
+
+        for(int i = 0; i < str.length(); i++) {
+            if(ch == str.charAt(i)) {
+                ++frequency;
+            }
+        }
+
+        System.out.println("Frequency of " + ch + " = " + frequency);
+    }
+}
