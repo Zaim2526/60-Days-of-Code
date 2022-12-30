f57b65b4a4d02a5ec6a973c159c2111728cc89df
Zamin Zehra <108755732+Zayam2526@users.noreply.github.com>
2022-12-30 21:43:08 +0530

    Create PrintArray.java

---
+++ PrintArray.java
@@ -0,0 +1,12 @@
+// Java Program to print an array using for loop.
+
+ class PrintArray {
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+
+        for (int element: array) {
+            System.out.println(element);
+        }
+    }
+}
