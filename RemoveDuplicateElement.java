e47982d5a0f4bf48ab533fd3c7e705de3807b261
Zamin Zehra <108755732+Zayam2526@users.noreply.github.com>
2023-01-03 23:22:08 +0530

    Create RemoveDuplicateElement.java

---
+++ RemoveDuplicateElement.java
@@ -0,0 +1,33 @@
+// Java Program to Remove Duplicate Elements from array.
+
+import java.util.Arrays;  
+public class RemoveDuplicateElement{  
+public static int removeDuplicateElements(int arr[], int n){  
+        if (n==0 || n==1){  
+            return n;  
+        }  
+        int[] temp = new int[n];  
+        int j = 0;  
+        for (int i=0; i<n-1; i++){  
+            if (arr[i] != arr[i+1]){  
+                temp[j++] = arr[i];  
+            }  
+         }  
+        temp[j++] = arr[n-1];     
+         
+        for (int i=0; i<j; i++){  
+            arr[i] = temp[i];  
+        }  
+        return j;  
+    }  
+       
+    public static void main (String[] args) {  
+        int arr[] = {10,70,30,90,20,20,40,40,70,50};//unsorted array  
+        Arrays.sort(arr);//sorting array  
+        int length = arr.length;  
+        length = removeDuplicateElements(arr, length);  
+        
+        for (int i=0; i<length; i++)  
+           System.out.print(arr[i]+" ");  
+    }  
+}  
