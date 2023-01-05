8f36d0e817956540699be3bcb839e633f3cdf4b2
Zamin Zehra <108755732+Zayam2526@users.noreply.github.com>
2023-01-05 23:03:06 +0530

    Create OctalToDecimal.java

---
+++ OctalToDecimal.java
@@ -0,0 +1,13 @@
+// Java Program to convert octal number into binary number.
+
+public class OctalToDecimal{  
+public static void main(String args[]){  
+
+   String octalString="121";  
+
+   int decimal=Integer.parseInt(octalString,8);  
+  
+   System.out.println(decimal);  
+  }
+    
+}  
