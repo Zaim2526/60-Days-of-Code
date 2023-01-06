18a1749ea332a0e4bcd090e5daa6396689c81435
Zamin Zehra <108755732+Zayam2526@users.noreply.github.com>
2023-01-06 22:48:01 +0530

    Create HexToDecimal.java

---
+++ HexToDecimal.java
@@ -0,0 +1,8 @@
+// Java Program to convert hexadecimal to decimal.
+public class HexToDecimal{  
+public static void main(String args[]){  
+    String hex="a";  
+     int decimal=Integer.parseInt(hex,16);  
+    System.out.println(decimal);  
+    }
+}  
