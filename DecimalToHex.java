701c8c9b8e165f29d61d00c3d51f526d197664a6
Zamin Zehra <108755732+Zayam2526@users.noreply.github.com>
2023-01-06 22:51:14 +0530

    Create DecimalToHex.java

---
+++ DecimalToHex.java
@@ -0,0 +1,9 @@
+// Java Program to convert decimal to hexadecimal.
+
+public class DecimalToHex{  
+public static void main(String args[]){  
+    System.out.println(Integer.toHexString(10));  
+    System.out.println(Integer.toHexString(15));  
+    System.out.println(Integer.toHexString(289));  
+     }
+ }  
