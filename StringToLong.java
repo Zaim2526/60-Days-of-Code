8729936d1114727e7869dacbf24579eccf71cde5
Zamin Zehra <108755732+Zayam2526@users.noreply.github.com>
2023-01-11 22:32:11 +0530

    Create StringToLong.java

---
+++ StringToLong.java
@@ -0,0 +1,9 @@
+// Java Program to convert String to Long.
+
+public class StringToLong{  
+ public static void main(String args[]){  
+   String s="9990449835";  
+   long l=Long.parseLong(s);  
+   System.out.println(l);  
+  }
+}  
