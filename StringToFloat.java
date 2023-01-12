917d4e68bb261572b8d1187d6021c0a6292d83b1
Zamin Zehra <108755732+Zayam2526@users.noreply.github.com>
2023-01-12 23:02:43 +0530

    Create StringToFloat.java

---
+++ StringToFloat.java
@@ -0,0 +1,9 @@
+// Java Program to convert String To Float.
+
+public class StringToFloat{  
+public static void main(String args[]){  
+    String s="24.6";  
+    float f=Float.parseFloat("23.6");  
+    System.out.println(f);  
+   }
+}  
