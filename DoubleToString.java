2986f2a1302613f34e0cdfd072100c40cd2dceba
Zamin Zehra <108755732+Zayam2526@users.noreply.github.com>
2023-01-13 19:59:28 +0530

    Create DoubleToString.java

---
+++ DoubleToString.java
@@ -0,0 +1,9 @@
+// Java program to convert Double to String.
+
+public class DoubleToString{  
+public static void main(String args[]){  
+     double d=12.3;  
+     String s=String.valueOf(d);  
+       System.out.println(s);  
+   }
+}  
