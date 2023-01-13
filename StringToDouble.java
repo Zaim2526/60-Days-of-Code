53c4c7cb518d3a5ec35e8b5446157cbf1a7cff57
Zamin Zehra <108755732+Zayam2526@users.noreply.github.com>
2023-01-13 19:57:37 +0530

    Create StringToDouble.java

---
+++ StringToDouble.java
@@ -0,0 +1,9 @@
+// Java Program to convert String to Double.
+
+public class StringToDouble{  
+public static void main(String args[]){  
+    String s="23.6";  
+    double d=Double.parseDouble("23.6");  
+    System.out.println(d);  
+    }
+}  
