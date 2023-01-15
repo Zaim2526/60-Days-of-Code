37a19d3b0caaa284fe8ceb5f8e3e853a12b2e739
Zamin Zehra <108755732+Zayam2526@users.noreply.github.com>
2023-01-15 21:43:02 +0530

    Create StringToChar.java

---
+++ StringToChar.java
@@ -0,0 +1,9 @@
+// Java Program to convert String To Char
+
+public class StringToChar{  
+public static void main(String args[]){  
+   String s="zamin";  
+   char c=s.charAt(0);//returns h  
+   System.out.println("1st character is: "+c);  
+   }
+}  
