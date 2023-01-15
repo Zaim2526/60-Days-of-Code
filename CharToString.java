d49064db2e6930a60d008a125d2c2fc1111a4dd5
Zamin Zehra <108755732+Zayam2526@users.noreply.github.com>
2023-01-15 21:40:35 +0530

    Create CharToString.java

---
+++ CharToString.java
@@ -0,0 +1,9 @@
+// Java Program to convert Char to String.
+
+public class CharToString{  
+public static void main(String args[]){  
+   char c='S';  
+   String s=String.valueOf(c);  
+   System.out.println("String is: "+s);  
+   }
+}  
