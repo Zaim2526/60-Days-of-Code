b3af067c77958aa675369c419cd0ca4b6867d9c0
Zamin Zehra <108755732+Zayam2526@users.noreply.github.com>
2023-01-16 19:24:08 +0530

    Create IntToLong.java

---
+++ IntToLong.java
@@ -0,0 +1,9 @@
+//Java Program to convert Int to Long.
+
+public class IntToLong{  
+public static void main(String args[]){  
+   int i=200;  
+   long l=i;  
+   System.out.println(l);  
+   }
+}  
