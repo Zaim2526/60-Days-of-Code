fad54da8acd113b7df95183617a4609a473133c2
Zamin Zehra <108755732+Zayam2526@users.noreply.github.com>
2023-01-16 19:27:18 +0530

    Create LongToInt.java

---
+++ LongToInt.java
@@ -0,0 +1,9 @@
+// Java Program to convert Long to Int.
+
+public class LongToInt{  
+public static void main(String args[]){  
+   long l=500;  
+    int i=(int)l;  
+     System.out.println(i);  
+   }
+}  
