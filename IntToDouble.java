e789d8dd29f2ae7ed5191fb9fcc068a9c5387597
Zamin Zehra <108755732+Zayam2526@users.noreply.github.com>
2023-01-17 22:14:29 +0530

    Create IntToDouble.java

---
+++ IntToDouble.java
@@ -0,0 +1,9 @@
+// Java Program to convert Int to Double 
+
+public class IntToDouble{  
+public static void main(String args[]){  
+    int i=200;  
+    double d=i;  
+    System.out.println(d);  
+   }
+}  
