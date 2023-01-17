9b56836fff99c04eee0c07b09acb1de2a349130f
Zamin Zehra <108755732+Zayam2526@users.noreply.github.com>
2023-01-17 22:16:05 +0530

    Create DoubleToInt.java

---
+++ DoubleToInt.java
@@ -0,0 +1,9 @@
+// Java Program to convert Double to Int
+
+public class DoubleToInt{  
+public static void main(String args[]){  
+    double d=10.5;  
+    int i=(int)d;  
+    System.out.println(i);  
+    }
+}  
