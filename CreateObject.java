1beed7268b3774f87f873f1b938cca572df1e193
Zamin Zehra <108755732+Zayam2526@users.noreply.github.com>
2023-01-27 21:23:24 +0530

    Create CreateObject.java

---
+++ CreateObject.java
@@ -0,0 +1,14 @@
+// Java Program to create an Object.
+
+public class CreateObject  
+{    
+ void show()    
+ {    
+   System.out.println(" Hii. I am zaim");    
+  }    
+   public static void main(String[] args)   
+   {       
+    CreateObject obj = new CreateObject();    
+    obj.show();    
+    }    
+}   
